532500286364d6efeec889201f7e77a51b9c4bf9
gianfranco <gmanieli5647@gmail.com>
2024-08-12 14:14:50 +1000

    objectify silly ultasonic
    wip

--- ultrasonic_sensor/ultra_test/ultra_test/ultra_test.h
+++ ultrasonic_sensor/ultra_test/ultra_test/ultra_test.h
@@ -1,9 +1,14 @@
 #ifndef ultra_test_h
 #define ultra_test_h
 
-const int trigPin = 9;
-const int echoPin = 10;
-long measureDuration();
-int calculateDistance(long duration);
+
+class ultra {
+private:
+public:
+    const int trigPin = 9;
+    const int echoPin = 10;
+    long measureDuration();
+    int calculateDistance(long duration);
+};
 
 #endif
