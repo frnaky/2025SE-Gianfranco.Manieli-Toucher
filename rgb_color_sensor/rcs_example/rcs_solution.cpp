57906732158494b76bc765a8c979f2d42353d2ad
xQzca <gianfranco.manieli@education.nsw.gov.au>
2024-08-14 15:16:48 +1000

    rgb colour sensor objectifcation
    test in class
    
    use of copilot to clean up previous header file

--- rgb_color_sensor/rcs_example/rcs_solution.cpp
+++ rgb_color_sensor/rcs_example/rcs_solution.cpp
@@ -1,17 +1,38 @@
 #include "rcs_solution.h"
 #include <Arduino.h.
 
-Adafruit_TCS34725::setup() {
-  Serial.begin(9600);
-  myservo.attach(9); //attach servo to pin 9
+rcs::rcs() : tcs(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X) {}
+
+void ColorSensor::init() {
   if (tcs.begin()) {
-    Serial.println("sensor detected");
+    Serial.println("colour sensor detect");
   } else {
-    Serial.println("sensot not found");
+    Serial.println("cannot find colour sensor");
     while (1);
   }
 }
 
-Adafruit_TCS34725::loop() {
-    
+void ColorSensor::readColor() {
+  tcs.getRawData(&r, &g, &b, nullptr);
+}
+
+uint16_t ColorSensor::getRed() {
+  return r;
+}
+
+uint16_t ColorSensor::getGreen() {
+  return g;
+}
+
+uint16_t ColorSensor::getBlue() {
+  return b;
+}
+
+void ColorSensor::printColor() {
+  Serial.print("Red: ");
+  Serial.print(r);
+  Serial.print(" Green: ");
+  Serial.print(g);
+  Serial.print(" Blue: ");
+  Serial.println(b);
 }
